f2579454bef4bddbda93b5fa245f58885124a7c1
Christian Prado <christian.prado@myparadigm.com>
2024-05-06 17:55:04 -0700

    update vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    lib: {
+      entry: 'src/index.ts',
+      name: 'MyLibrary',
+      formats: ['es', 'cjs'],
+      fileName: format => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
+  plugins: [dts({
+    outDir: 'dist',
+    staticImport: true
+  })]
 })
