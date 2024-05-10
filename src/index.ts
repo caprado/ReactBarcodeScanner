08484563767fc91654af97c6e2d31c6dea9fc811
Christian Prado <christian.prado@myparadigm.com>
2024-05-09 19:20:11 -0700

    expose types for custom finder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Scanner } from './components/Scanner'
 import { useContinuousScanner } from './hooks/useContinuousScanner'
 import { useDeviceList } from './hooks/useDeviceList'
+import { FinderProps } from './types'
+import { ScannerProps } from './types'
 
-export { Scanner, useDeviceList, useContinuousScanner }
+export { ScannerProps, FinderProps, Scanner, useDeviceList, useContinuousScanner }
